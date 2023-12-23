90d922da77fd2b0c2859d2ed404c60ba50fccf28
404salad <404salad@gmail.com>
2023-12-23 14:52:22 +0530

    removed unneccesary files

--- 48.rs
+++ 48.rs
@@ -1,12 +1,20 @@
-
-
 fn main() {
     println!("whta");
     let mut sum:u128 = 0;
-    for i in 1..10 {
-        for j in 1..10 {
-            println!("{}", (i as u128).pow(j as u32));
+    let mut product:u128 = 1;
+    for i in 1..1000 {
+        for j in 0..i {
+            product = product*i;
+            if product>100000_000_000 {
+                product%=100000_000_000
+            }
+        }
+        sum+=product;
+        if sum>100000_000_000 {
+            sum%=100000_000_000
         }
     }
+    println!("{}",sum);
+    // 8945256360
 }
 
